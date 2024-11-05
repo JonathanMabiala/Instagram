a18dd4d5a5276b3bb41e030a4b877afd39447f86
Developer <developer@Jonathans-Mac-mini.local>
2024-11-05 17:51:29 -0500

    deepLinking android

--- src/navigation/ProfileStackNavigator.tsx
+++ src/navigation/ProfileStackNavigator.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ProfileScreen from '../screens/ProfileScreen';
 import EditProfileScreen from '../screens/EditProfileScreen';
+import {ProfileStackNavigatorParamList} from './types';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<ProfileStackNavigatorParamList>();
 
 const ProfileStackNavigator = () => {
   return (
