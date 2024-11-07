8923c3efeeb4db157678cccefc2b762dd65ef4c0
JonathanMabiala <jonathan.mabiala@outlook.com>
2024-11-07 16:44:21 -0500

    renaming the application

--- android/app/src/main/java/dev/dopecode/photos/MainApplication.kt
+++ android/app/src/main/java/dev/dopecode/photos/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.instagram
+package dev.dopecode.photos
 import android.content.res.Configuration
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
--- ios/dopeCodePhotosTests/dopeCodePhotosTests.m
+++ ios/dopeCodePhotosTests/dopeCodePhotosTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface instagramTests : XCTestCase
+@interface dopeCodePhotosTests : XCTestCase
 
 @end
 
-@implementation instagramTests
+@implementation dopeCodePhotosTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
